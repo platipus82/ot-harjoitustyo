4dae7a64b7225ed57af78909739e402f2aca9508
Oleg Kambur <oleg.kambur@gmail.com>
2023-04-22 18:13:48 +0300

    quality assurance

--- src/flashcards.py
+++ src/flashcards.py
@@ -20,7 +20,7 @@ class App:
         self.output_allowed=output_allowed
         self.input_dir = ""
         self.input_path = ""
-        if not self.default_input: 
+        if not self.default_input:
             self.__ui = UI(use_default_input=self.default_input, output_allowed=output_allowed)
         self.exit = False
         self.data = []
@@ -51,9 +51,14 @@ class App:
     def set_input_path_default(self):
         filelist = self.filelist
         filelist_description = "Currently available files are: " + "\n"
-        for i in range(0, len(filelist)):
-            filepart =  str(i) + ": " + filelist[i] + "\n"
+        #for i in range(0, len(filelist)):
+        #    filepart =  str(i) + ": " + filelist[i] + "\n"
+        #    filelist_description = filelist_description+filepart
+        for i, filename in enumerate(filelist):
+            filepart =  str(i) + ": " + filename + "\n"
             filelist_description = filelist_description+filepart
+
+
         new_part = "Currently running in default input mode and will use the first available file."
         filelist_description = filelist_description + new_part
         self.input_path = self.input_dir + "/" + self.filelist[0]
@@ -71,37 +76,7 @@ class App:
     def set_input_path(self):
         pth= self.__ui.ask_for_input_file("") #GUI_input_file_selection()
         self.input_path = pth
-        
-    def set_input_path_v1(self):
-        #self.input_path = os.getcwd() + "\inputs\\"
-        filelist = self.filelist
-        filelist_description = "Currently available files are: " + "\n"
-        for i in range(0, len(filelist)):
-            filepart =  str(i) + ": " + filelist[i] + "\n"
-            filelist_description = filelist_description+filepart
-        new_part =  "Please, choose correct input file by typing the corresponding number..."
-        filelist_description = filelist_description + new_part
-        #print(filelist_description)
 
-        while True:
-            num = self.__ui.ask_for_input(output_text=filelist_description)
-            try:
-                num = int(num)
-                if 0 <= num < len(filelist):
-                    break
-                else:
-                    msg = "You must choose an integer between 0 and " + str(len(filelist))
-                    self.__ui.show_output(output_text=msg)
-            except ValueError:
-                msg = "You must choose an integer between 0 and " + str(len(filelist))
-                self.__ui.show_output(output_text=msg)
-        #print(f"You have chosen file {filelist[num]} ({num})")
-        msg = f"You have chosen file {filelist[num]} ({num})"
-        self.__ui.show_output(output_text=msg)
-
-        self.input_path = self.input_dir + "/" + filelist[num]
-        if not os.path.isfile(self.input_path):
-            self.input_path = self.input_dir + "\\" + filelist[num]
 
     def get_input_data(self):
         try:
@@ -128,13 +103,15 @@ class Play:
 
     def ask(self):
         lst = self.game.data
-        for i in range(0, len(lst)):
-            y = lst[i].split(";")
+        #for i in range(0, len(lst)):
+        #    y = lst[i].split(";")
+        for i, entry in enumerate(lst):
+            y = entry.split(";")
             question = y[0]
             answer = y[1]
             msg_pt1 = "Please, press proceed to see the correct answer" + "\n"
             msg_pt2 = "Press exit to exit the application" + "\n"
-            msg = "Question: " + question + "\n" + msg_pt1 + msg_pt2            
+            msg = "Question: " + question + "\n" + msg_pt1 + msg_pt2
             resp = self.__ui.ask_for_input(output_text=msg)
 
             if resp=="x":
@@ -159,9 +136,9 @@ def main():
     Play()
 
 if __name__ == "__main__":
-    import os
-    from ui.ui import UI  
-    from ui.ui import GUI
-    from ui.ui import GUI_input_file_selection
+    #import os
+    from ui.ui import UI
+    #from ui.ui import GUI
+    #from ui.ui import GUI_input_file_selection
 
     main()
