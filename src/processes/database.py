efaa6c5a9f30060519eb3d66c1d859eb3eadc851
Oleg Kambur <oleg.kambur@gmail.com>
2023-05-15 15:45:24 +0300

    final updates 15.5 pt1

--- src/processes/database.py
+++ src/processes/database.py
@@ -11,22 +11,23 @@ import os
 import csv
 import sqlite3
 
+
 class Database:
     def __init__(self, output_allowed=True):
-        #print("Class Database")
+        # print("Class Database")
         self.db_dir = os.getcwd() + "\src\repositories\\"
         if not os.path.isfile(self.db_dir):
-            self.db_dir = os.getcwd() + "/src/repositories/"        
+            self.db_dir = os.getcwd() + "/src/repositories/"
         self.output_allowed = output_allowed
-        
+
         # .csv database
         self.db_path = self.db_dir + "csv_database.csv"
         self.data = []
         if not os.path.isfile(self.db_path):
             self.make_new_db_file()
-        #print(self.db_dir)
-        #print(self.db_path)
-        
+        # print(self.db_dir)
+        # print(self.db_path)
+
         # SQL-database
         self.sql_db_path = self.db_dir + "database.db"
         if not os.path.isfile(self.sql_db_path):
@@ -42,34 +43,37 @@ class Database:
         all = self.give_summary_of_db()
         msg = this + all
 
-        self.summary=msg
+        self.summary = msg
 
     def make_new_sql_db_file(self):
         db = sqlite3.connect(self.sql_db_path)
         db.isolation_level = None
         c = db.cursor()
-        
+
         # Step 1: creating an empty table
-        #print("...1: Creating an empty table if one does not exist yet.")
+        # print("...1: Creating an empty table if one does not exist yet.")
         c.execute("CREATE TABLE IF NOT EXISTS Records (id TEXT NOT NULL, user TEXT NOT NULL, deck TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, elapsed_time TEXT NOT NULL, questions_n_total INTEGER, questions_n_checked INTEGER, questions_n_answered INTEGER, questions_n_correct INTEGER, questions_percent_correct REAL);")
-  
+
     def make_db_dir(self):
-        #check if the input folder exists and has files
-        if os.path.exists(self.db_dir) and os.path.isdir(self.db_dir): 
+        # check if the input folder exists and has files
+        if os.path.exists(self.db_dir) and os.path.isdir(self.db_dir):
             pass
         else:
             os.mkdir(self.db_dir)
 
     def tell_db_colnames(self):
-        cols = [["user", "deck", "start_time", "end_time", "elapsed_time", "questions_n_total", "questions_n_checked", "questions_n_answered", "questions_n_correct", "questions_percent_correct"]]
+        cols = [["user", "deck", "start_time", "end_time", "elapsed_time", "questions_n_total",
+                 "questions_n_checked", "questions_n_answered", "questions_n_correct", "questions_percent_correct"]]
         return cols
-        
+
     def make_new_db_file(self):
-        self.write_file(output_csv_file=self.db_path, rows_to_write=self.tell_db_colnames(), mode="w")
-    
+        self.write_file(output_csv_file=self.db_path,
+                        rows_to_write=self.tell_db_colnames(), mode="w")
+
     def write_to_db(self, output_csv_file="", rows_to_write=[], mode="a"):
-        self.write_file(output_csv_file=self.db_path, rows_to_write=rows_to_write, mode="a")
-    
+        self.write_file(output_csv_file=self.db_path,
+                        rows_to_write=rows_to_write, mode="a")
+
     def write_to_sql_db(self, rows_to_write=[]):
         headers = self.tell_db_colnames()[0]
         data = []
@@ -77,39 +81,38 @@ class Database:
             id = x[2]
             x = [id] + x
             data.append(x)
-            
-        #for x in data:
+
+        # for x in data:
         #    print(x)
         db = sqlite3.connect(self.sql_db_path)
         db.isolation_level = None
         c = db.cursor()
         c.execute("BEGIN")
-        
-        # id, user, deck, start_time, end_time, elapsed_time, questions_n_total, 
-        # questions_n_answered, questions_n_correct, questions_percent_correct 
+
+        # id, user, deck, start_time, end_time, elapsed_time, questions_n_total,
+        # questions_n_answered, questions_n_correct, questions_percent_correct
         c.execute("CREATE TABLE IF NOT EXISTS Records (id STRING PRIMARY KEY, user TEXT NOT NULL, deck TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, elapsed_time TEXT NOT NULL, questions_n_total INTEGER, questions_n_checked INTEGER, questions_n_answered INTEGER, questions_n_correct INTEGER, questions_percent_correct REAL);")
-        c.executemany("INSERT INTO Records (id, user, deck, start_time, end_time, elapsed_time, questions_n_total, questions_n_checked, questions_n_answered, questions_n_correct, questions_percent_correct ) VALUES (?,?,?,?,?,?,?,?,?,?,?);", data) 
+        c.executemany("INSERT INTO Records (id, user, deck, start_time, end_time, elapsed_time, questions_n_total, questions_n_checked, questions_n_answered, questions_n_correct, questions_percent_correct ) VALUES (?,?,?,?,?,?,?,?,?,?,?);", data)
         c.execute("COMMIT")
-        
-        
+
     def write_file(self, output_csv_file="", rows_to_write=[], mode="a"):
         try:
             with open(output_csv_file, mode, newline='') as myfile:
-                filewriter = csv.writer(myfile, quoting=csv.QUOTE_NONE, delimiter=";")
+                filewriter = csv.writer(
+                    myfile, quoting=csv.QUOTE_NONE, delimiter=";")
                 for r in rows_to_write:
-                    filewriter.writerow(r)       
+                    filewriter.writerow(r)
             return True
         except (OSError):
-            return False       
+            return False
 
-    
     def get_db_data(self):
         try:
             with open(self.db_path, "r", encoding="utf-8") as infile:
                 lines = []
-                i=0
+                i = 0
                 for line in infile:
-                    if line != "" :
+                    if line != "":
                         items = line.split(";")
                         lines.append(items)
                         i += 1
@@ -118,7 +121,7 @@ class Database:
         except OSError:
             self.data = None
             return False
-        
+
     def get_sql_db_data(self):
         db = sqlite3.connect(self.sql_db_path)
         db.isolation_level = None
@@ -126,109 +129,109 @@ class Database:
         c.execute("BEGIN")
         data = c.execute("SELECT * FROM Records;").fetchall()
         db.close()
-        listat =[]
+        listat = []
         for d in data:
             lista = []
             for x in d:
                 lista.append(str(x))
             lista.pop(0)
             listat.append(lista)
-        self.data = listat 
+        self.data = listat
 
-        
-    def give_summary_data(self):      
+    def give_summary_data(self):
         self.get_sql_db_data()
         db_db = self.data
         all_results = self.data
         return all_results
-    
+
     def give_summary(self):
         return self.summary
 
-
-    
     def give_summary_of_last_session(self):
         msg = ""
-        #print(self.data)
+        # print(self.data)
         last = self.data[-1]
         last = last
-        msg = msg + "Summary of this session:" +"\n"        
+        msg = msg + "Summary of this session:" + "\n"
         cols = self.tell_db_colnames()[0]
-        for i,entry in enumerate(last):
-            #print(cols[i] + ": " + entry)
-            msg = msg + cols[i] + ": " + entry +"\n" 
+        for i, entry in enumerate(last):
+            # print(cols[i] + ": " + entry)
+            msg = msg + cols[i] + ": " + entry + "\n"
         return msg
-    
+
     def give_summary_of_db(self):
         dat = self.data
         msg = ""
-        msg = msg + "Currently database contains: " +"\n" 
+        msg = msg + "Currently database contains: " + "\n"
 
         # Sessions
-        msg = msg + str(len(dat)) + " recorded sessions" +"\n" 
-        
+        msg = msg + str(len(dat)) + " recorded sessions" + "\n"
+
         # n of users
         users = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 user = entry[0]
                 users.append(user)
         users = list(set(users))
-        msg = msg + str(len(users)) + " different users" +"\n" 
+        msg = msg + str(len(users)) + " different users" + "\n"
 
-        
         # n of input files
         infiles = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 infile = entry[1]
                 infiles.append(infile)
         infiles = list(set(infiles))
-        msg = msg + str(len(infiles)) + " different decks (input files) have been used" +"\n" 
-        
+        msg = msg + str(len(infiles)) + \
+            " different decks (input files) have been used" + "\n"
+
         # total time
         times = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 used_time = entry[4]
                 times.append(int(used_time))
         total_time = sum(times)
-        msg = msg + str(total_time) + " seconds has been spent in total"+"\n" 
-        cols = [["user", "deck", "start_time", 
-                 "end_time", "elapsed_time", "questions_n_total", 
-                 "questions_n_checked", "questions_n_answered", "questions_n_correct", 
+        msg = msg + str(total_time) + " seconds has been spent in total"+"\n"
+        cols = [["user", "deck", "start_time",
+                 "end_time", "elapsed_time", "questions_n_total",
+                 "questions_n_checked", "questions_n_answered", "questions_n_correct",
                  "questions_percent_correct"]]
-
 
         # total checked questions
         questions = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 nquest = entry[6]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + " questions have been viewed in total"+"\n" 
-                
+        msg = msg + str(sum(questions)) + \
+            " questions have been viewed in total"+"\n"
+
         # total answered questions
         questions = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 nquest = entry[7]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + " questions have been answered in total"+"\n" 
-        
+        msg = msg + str(sum(questions)) + \
+            " questions have been answered in total"+"\n"
+
         # correctly answered questions
         questions = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 nquest = entry[8]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + " questions have been answered correctly"+"\n" 
-        
-        # percentage of correct 
+        msg = msg + str(sum(questions)) + \
+            " questions have been answered correctly"+"\n"
+
+        # percentage of correct
         results = []
-        for i,entry in enumerate(dat):
-            if i!=0:
+        for i, entry in enumerate(dat):
+            if i != 0:
                 x = entry[9]
                 results.append(float(x))
-        msg = msg + str(sum(results)/len(results)) + " was the average percent of correct answers"+"\n" 
+        msg = msg + str(sum(results)/len(results)) + \
+            " was the average percent of correct answers"+"\n"
         return msg
--- src/processes/play.py
+++ src/processes/play.py
@@ -7,83 +7,87 @@ Created on 12 May 2023
 @author: ok
 '''
 
-#import os
+# import os
 import datetime
 from processes.app import App
 from processes.database import Database
 from ui.ui import UI
 
-#from ui.gui import GUI
-#from ui.gui_input_file_selection import GUI_input_file_selection
+# from ui.gui import GUI
+# from ui.gui_input_file_selection import GUI_input_file_selection
+
+
 class Play:
     def __init__(self, use_default_input=True, output_allowed=False):
-        self.use_default_input=use_default_input 
-        self.output_allowed=output_allowed
+        self.use_default_input = use_default_input
+        self.output_allowed = output_allowed
         self.start_time = datetime.datetime.now()
-        self.end_time=""
+        self.end_time = ""
         self.mode = None
         self.__exit = False
-        self.game = App(use_default_input=use_default_input, output_allowed=output_allowed)
+        self.game = App(use_default_input=use_default_input,
+                        output_allowed=output_allowed)
         self.questions_n_total = len(self.game.data)
         self.questions_n_checked = 0
         self.questions_n_answered = 0
         self.questions_n_correct = 0
-        
-        self.__ui = UI(use_default_input=use_default_input, output_allowed=output_allowed)
+
+        self.__ui = UI(use_default_input=use_default_input,
+                       output_allowed=output_allowed)
         self.set_mode()
 
         self.db = Database(output_allowed=output_allowed)
-        self.__exit=self.game.exit
-        if self.__exit == False and self.output_allowed==True:
+        self.__exit = self.game.exit
+        if self.__exit == False and self.output_allowed == True:
             self.ask()
-            #self.ask_with_answers()
+            # self.ask_with_answers()
 
-        #print("questions over, saving results")
-        if self.mode==0:
-           self.__exit=True
-        #self.save_results2()
+        # print("questions over, saving results")
+        if self.mode == 0:
+            self.__exit = True
+        # self.save_results2()
         if self.__exit == True:
             self.end_time = datetime.datetime.now()
             self.elapsed_time = self.end_time-self.start_time
             self.save_results()
-            #self.save_results_to_csv()
-            #self.db.print_summary()
+            # self.save_results_to_csv()
+            # self.db.print_summary()
             if self.output_allowed:
                 msg = self.db.give_summary()
                 self.__ui.show_output(output_text=msg)
-        
 
     def set_mode(self):
-        if self.use_default_input==False and self.output_allowed==True:
-            resp=self.__ui.set_mode()
+        if self.use_default_input == False and self.output_allowed == True:
+            resp = self.__ui.set_mode()
             if resp == "x":
                 self.__exit = True
             else:
                 self.mode = resp
         else:
-            self.mode=0
+            self.mode = 0
 
     def ask(self):
         lst = self.game.data
-        while not self.__exit: 
+        while not self.__exit:
             for i, entry in enumerate(lst):
                 y = entry.split(";")
                 question = y[0]
                 answer = y[1]
-                if i!=len(lst)-1:
+                if i != len(lst)-1:
                     last_question = False
-                else: 
+                else:
                     last_question = True
                 self.questions_n_checked += 1
-                if self.output_allowed==True:
-                    resp = self.__ui.ask(question=question, answer=answer, last_question=last_question)
+                if self.output_allowed == True:
+                    resp = self.__ui.ask(
+                        question=question, answer=answer, last_question=last_question)
                     self.__exit = self.__ui.exit
-                    if self.__ui.last_question_correct==True:
+                    if self.__ui.last_question_correct == True:
                         self.questions_n_correct += 1
                     self.questions_n_answered += 1
                     if self.__exit:
                         break
-            self.__exit=True
+            self.__exit = True
 
     '''def save_results_to_csv(self):
         answer = self.__ui.ask_for_text(output_text="Please, type your name")
@@ -120,27 +124,27 @@ class Play:
         self.db.write_file(output_csv_file=output_file_name_full, rows_to_write=dat, mode="w")
         '''
 
-
     def save_results(self):
         usr_name = self.__ui.ask_for_text(output_text="Please, type your name")
         infile = self.game.input_file_name
 
         # Output
-        #cols = [["user", "deck", "start_time", "end_time","elapsed_time"  "questions_n_total", "questions_n_answered", "questions_n_correct", "questions_percent_correct"]]
+        # cols = [["user", "deck", "start_time", "end_time","elapsed_time"  "questions_n_total", "questions_n_answered", "questions_n_correct", "questions_percent_correct"]]
         # time
         tstart = str(self.start_time)
         tend = str(self.end_time)
-        telaps = self.elapsed_time.seconds      
-        
+        telaps = self.elapsed_time.seconds
+
         # n questions
-        qntot = self.questions_n_total 
-        qnche = self.questions_n_checked 
-        qnans = self.questions_n_answered 
-        qncor = self.questions_n_correct 
-        qpcor = qncor/(qntot/100) #"questions_percent_correct"
-       
-        
-        dat = [[usr_name, infile, tstart, tend, telaps, qntot, qnche, qnans, qncor, qpcor]]
+        qntot = self.questions_n_total
+        qnche = self.questions_n_checked
+        qnans = self.questions_n_answered
+        qncor = self.questions_n_correct
+        qpcor = qncor/(qntot/100)  # "questions_percent_correct"
+
+        dat = [[usr_name, infile, tstart, tend,
+                telaps, qntot, qnche, qnans, qncor, qpcor]]
         if self.output_allowed:
-            self.db.write_to_db(output_csv_file="", rows_to_write=dat, mode="a")  
-            self.db.write_to_sql_db(rows_to_write=dat)  
+            self.db.write_to_db(output_csv_file="",
+                                rows_to_write=dat, mode="a")
+            self.db.write_to_sql_db(rows_to_write=dat)
