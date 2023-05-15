0c58c0f567839fd791ca9c08c205a9156bf340ff
Oleg Kambur <oleg.kambur@gmail.com>
2023-05-15 16:01:19 +0300

    updated database.py

--- src/processes/database.py
+++ src/processes/database.py
@@ -39,20 +39,26 @@ class Database:
 
     def make_summary(self):
         results = self.give_summary_data()
-        this = self.give_summary_of_last_session()
-        all = self.give_summary_of_db()
-        msg = this + all
-
+        last_session = self.give_summary_of_last_session()
+        whole_database = self.give_summary_of_db()
+        msg = last_session + whole_database
         self.summary = msg
 
     def make_new_sql_db_file(self):
-        db = sqlite3.connect(self.sql_db_path)
-        db.isolation_level = None
-        c = db.cursor()
+        database = sqlite3.connect(self.sql_db_path)
+        database.isolation_level = None
+        cursor = database.cursor()
 
         # Step 1: creating an empty table
         # print("...1: Creating an empty table if one does not exist yet.")
-        c.execute("CREATE TABLE IF NOT EXISTS Records (id TEXT NOT NULL, user TEXT NOT NULL, deck TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, elapsed_time TEXT NOT NULL, questions_n_total INTEGER, questions_n_checked INTEGER, questions_n_answered INTEGER, questions_n_correct INTEGER, questions_percent_correct REAL);")
+        part1 = "CREATE TABLE IF NOT EXISTS Records "
+        part2 = "(id TEXT NOT NULL, user TEXT NOT NULL, deck TEXT NOT NULL, "
+        part3 = "start_time TEXT NOT NULL, end_time TEXT NOT NULL, "
+        part4 = "elapsed_time TEXT NOT NULL, questions_n_total INTEGER, "
+        part5 = "questions_n_checked INTEGER, questions_n_answered INTEGER, "
+        part6 = "questions_n_correct INTEGER, questions_percent_correct REAL);"
+        whole_command = part1+part2+part3+part4+part5+part6
+        cursor.execute(whole_command)
 
     def make_db_dir(self):
         # check if the input folder exists and has files
@@ -62,48 +68,63 @@ class Database:
             os.mkdir(self.db_dir)
 
     def tell_db_colnames(self):
-        cols = [["user", "deck", "start_time", "end_time", "elapsed_time", "questions_n_total",
-                 "questions_n_checked", "questions_n_answered", "questions_n_correct", "questions_percent_correct"]]
+        cols = ["user", "deck", "start_time", "end_time",
+                "elapsed_time", "questions_n_total"]
+        cols = cols + ["questions_n_checked", "questions_n_answered",
+                       "questions_n_correct", "questions_percent_correct"]
+        cols = [cols]
         return cols
 
     def make_new_db_file(self):
         self.write_file(output_csv_file=self.db_path,
                         rows_to_write=self.tell_db_colnames(), mode="w")
 
-    def write_to_db(self, output_csv_file="", rows_to_write=[], mode="a"):
+    def write_to_db(self, output_csv_file="", rows_to_write=None, mode="a"):
         self.write_file(output_csv_file=self.db_path,
-                        rows_to_write=rows_to_write, mode="a")
+                        rows_to_write=rows_to_write, mode=mode)  # mode="a"
 
-    def write_to_sql_db(self, rows_to_write=[]):
-        headers = self.tell_db_colnames()[0]
+    def write_to_sql_db(self, rows_to_write=None):
         data = []
         for x in rows_to_write:
-            id = x[2]
-            x = [id] + x
+            primary_key = x[2]
+            x = [primary_key] + x
             data.append(x)
 
         # for x in data:
         #    print(x)
-        db = sqlite3.connect(self.sql_db_path)
-        db.isolation_level = None
-        c = db.cursor()
-        c.execute("BEGIN")
+        database = sqlite3.connect(self.sql_db_path)
+        database.isolation_level = None
+        cursor = database.cursor()
+
+        cursor.execute("BEGIN")
 
         # id, user, deck, start_time, end_time, elapsed_time, questions_n_total,
         # questions_n_answered, questions_n_correct, questions_percent_correct
-        c.execute("CREATE TABLE IF NOT EXISTS Records (id STRING PRIMARY KEY, user TEXT NOT NULL, deck TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, elapsed_time TEXT NOT NULL, questions_n_total INTEGER, questions_n_checked INTEGER, questions_n_answered INTEGER, questions_n_correct INTEGER, questions_percent_correct REAL);")
-        c.executemany("INSERT INTO Records (id, user, deck, start_time, end_time, elapsed_time, questions_n_total, questions_n_checked, questions_n_answered, questions_n_correct, questions_percent_correct ) VALUES (?,?,?,?,?,?,?,?,?,?,?);", data)
-        c.execute("COMMIT")
+        whole_command = "CREATE TABLE IF NOT EXISTS Records (id STRING PRIMARY KEY, " + \
+            "start_time TEXT NOT NULL, end_time TEXT NOT NULL, elapsed_time TEXT NOT NULL, " + \
+            "questions_n_total INTEGER, questions_n_checked INTEGER, " + \
+            "questions_n_answered INTEGER, " + \
+            "questions_n_correct INTEGER, " + \
+            "questions_percent_correct REAL);"
+        cursor.execute(whole_command)
 
-    def write_file(self, output_csv_file="", rows_to_write=[], mode="a"):
+        whole_command = "INSERT INTO Records (" +\
+            "id, user, deck, start_time, end_time, elapsed_time, questions_n_total," +\
+            "questions_n_checked, questions_n_answered, questions_n_correct, " +\
+            "questions_percent_correct )" +\
+            "VALUES (?,?,?,?,?,?,?,?,?,?,?);"
+        cursor.executemany(whole_command, data)
+        cursor.execute("COMMIT")
+
+    def write_file(self, output_csv_file="", rows_to_write=None, mode="a"):
         try:
-            with open(output_csv_file, mode, newline='') as myfile:
+            with open(output_csv_file, mode, newline='', encoding="utf-8") as myfile:
                 filewriter = csv.writer(
                     myfile, quoting=csv.QUOTE_NONE, delimiter=";")
-                for r in rows_to_write:
-                    filewriter.writerow(r)
+                for row in rows_to_write:
+                    filewriter.writerow(row)
             return True
-        except (OSError):
+        except OSError:
             return False
 
     def get_db_data(self):
@@ -123,24 +144,23 @@ class Database:
             return False
 
     def get_sql_db_data(self):
-        db = sqlite3.connect(self.sql_db_path)
-        db.isolation_level = None
-        c = db.cursor()
-        c.execute("BEGIN")
-        data = c.execute("SELECT * FROM Records;").fetchall()
-        db.close()
+        database = sqlite3.connect(self.sql_db_path)
+        database.isolation_level = None
+        cursor = database.cursor()
+        cursor.execute("BEGIN")
+        data = cursor.execute("SELECT * FROM Records;").fetchall()
+        database.close()
         listat = []
-        for d in data:
+        for x in data:
             lista = []
-            for x in d:
-                lista.append(str(x))
+            for y in x:
+                lista.append(str(y))
             lista.pop(0)
             listat.append(lista)
         self.data = listat
 
     def give_summary_data(self):
         self.get_sql_db_data()
-        db_db = self.data
         all_results = self.data
         return all_results
 
@@ -149,9 +169,8 @@ class Database:
 
     def give_summary_of_last_session(self):
         msg = ""
-        # print(self.data)
+        #print(self.data)
         last = self.data[-1]
-        last = last
         msg = msg + "Summary of this session:" + "\n"
         cols = self.tell_db_colnames()[0]
         for i, entry in enumerate(last):
@@ -159,14 +178,7 @@ class Database:
             msg = msg + cols[i] + ": " + entry + "\n"
         return msg
 
-    def give_summary_of_db(self):
-        dat = self.data
-        msg = ""
-        msg = msg + "Currently database contains: " + "\n"
-
-        # Sessions
-        msg = msg + str(len(dat)) + " recorded sessions" + "\n"
-
+    def describe_n_of_users(self, dat):
         # n of users
         users = []
         for i, entry in enumerate(dat):
@@ -174,8 +186,10 @@ class Database:
                 user = entry[0]
                 users.append(user)
         users = list(set(users))
-        msg = msg + str(len(users)) + " different users" + "\n"
+        msg = str(len(users)) + " different users" + "\n"
+        return msg
 
+    def describe_n_of_input_files(self, dat):
         # n of input files
         infiles = []
         for i, entry in enumerate(dat):
@@ -183,9 +197,11 @@ class Database:
                 infile = entry[1]
                 infiles.append(infile)
         infiles = list(set(infiles))
-        msg = msg + str(len(infiles)) + \
+        msg = str(len(infiles)) + \
             " different decks (input files) have been used" + "\n"
+        return msg
 
+    def describe_total_time(self, dat):
         # total time
         times = []
         for i, entry in enumerate(dat):
@@ -193,45 +209,88 @@ class Database:
                 used_time = entry[4]
                 times.append(int(used_time))
         total_time = sum(times)
-        msg = msg + str(total_time) + " seconds has been spent in total"+"\n"
-        cols = [["user", "deck", "start_time",
-                 "end_time", "elapsed_time", "questions_n_total",
-                 "questions_n_checked", "questions_n_answered", "questions_n_correct",
-                 "questions_percent_correct"]]
+        msg = str(total_time) + " seconds has been spent in total"+"\n"
+        # cols = [["user", "deck", "start_time","end_time", "elapsed_time", "questions_n_total", "questions_n_checked", "questions_n_answered", "questions_n_correct","questions_percent_correct"]]
+        return msg
 
+    def total_checked_questions(self, dat):
         # total checked questions
         questions = []
         for i, entry in enumerate(dat):
             if i != 0:
                 nquest = entry[6]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + \
+        msg = str(sum(questions)) + \
             " questions have been viewed in total"+"\n"
+        return msg
 
+    def total_answered_questions(self, dat):
         # total answered questions
         questions = []
         for i, entry in enumerate(dat):
             if i != 0:
                 nquest = entry[7]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + \
+        msg = str(sum(questions)) + \
             " questions have been answered in total"+"\n"
+        return msg
 
+    def correctly_answered_questions(self, dat):
         # correctly answered questions
         questions = []
         for i, entry in enumerate(dat):
             if i != 0:
                 nquest = entry[8]
                 questions.append(int(nquest))
-        msg = msg + str(sum(questions)) + \
+        msg = str(sum(questions)) + \
             " questions have been answered correctly"+"\n"
+        return msg
 
+    def percentage_of_correct(self, dat):
         # percentage of correct
         results = []
         for i, entry in enumerate(dat):
             if i != 0:
                 x = entry[9]
                 results.append(float(x))
-        msg = msg + str(sum(results)/len(results)) + \
+        msg = str(sum(results)/len(results)) + \
             " was the average percent of correct answers"+"\n"
+        return msg
+
+    def give_summary_of_db(self):
+        dat = self.data
+        msg = "Currently database contains: " + "\n"
+
+        # Sessions
+        """
+        msg = msg + str(len(dat)) + " recorded sessions" + "\n" + \
+            describe_n_of_users(dat) + \
+            describe_n_of_input_files(dat) + \
+            describe_total_time(dat) + \
+            total_checked_questions(dat) + \
+            total_answered_questions(dat) + \
+            correctly_answered_questions(dat)
+        """
+
+        # Function names
+        #my_functions = [describe_n_of_users.__name__, describe_n_of_input_files.__name__, describe_total_time.__name__, total_checked_questions.__name__,total_answered_questions.__name__, correctly_answered_questions.__name__]
+        #my_functions = [ describe_n_of_users(), \
+        #    describe_n_of_input_files(), \
+        #    describe_total_time(),  \
+        #    total_checked_questions(),\
+        #    total_answered_questions(), \
+        #    correctly_answered_questions()]
+        #for x in my_functions:
+        # https://stackoverflow.com/questions/5461571/call-list-of-function-using-list-comprehension
+        # [f() for f in fl]
+        msg = msg + str(len(dat)) + " recorded sessions" + "\n" + \
+            self.describe_n_of_users(dat) + \
+            self.describe_n_of_input_files(dat) + \
+            self.describe_total_time(dat) + \
+            self.total_checked_questions(dat) + \
+            self.total_answered_questions(dat) + \
+            self.correctly_answered_questions(dat)
+
+        #msg = [msg+f() for f in my_functions]
+
         return msg
